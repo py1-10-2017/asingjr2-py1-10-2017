106fa58a48ff0ef8230d63af3b127712307300b8
asingjr2 <asingjr2@gmail.com>
2017-11-30 23:18:51 -0500

    adding val_log_reg

---
+++ val_log_reg/basic_log_reg/apps/val_log_reg/models.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+
+
+class AllManager(models.Manager):
+    def registration_validation(self, postData):
+        error = {}
+        if len(postData['name']) < 2:
+            error['name'] = "Name too short"
+        if len(postData['alias']) < 2:
+            error['alias'] = "Username too short"
+        if not EMAIL_REGEX.match(postData['email']):
+            # MUST BE REFERENCED CORRECTLY
+            error['email'] = "Email is not valid email"
+        if len(postData['password']) < 2:
+            error['password'] = 'Password must be at least 2 characters'
+        if postData['confirm_password'] != postData['password']:
+            error['confirm_password'] = "Confirm password does not match password"
+        return error
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200, default='person')
+    alias = models.CharField(max_length=200, default='random_alias')
+    email = models.CharField(max_length=200, default='random_email')
+    password = models.TextField()
+    confirm_password = models.TextField()
+    dob = models.CharField(max_length=100, default='add dob')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = AllManager()
+
+    def __str__(self):
+        return 'Name = {}, Email ={}, Alias = {}'.format(self.name, self.email, self.alias)
